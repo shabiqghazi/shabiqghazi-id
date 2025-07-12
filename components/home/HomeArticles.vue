55b7efefe6053645299cb6bcd655fc337aee9a3d
Shabiq Ghazi Arkaan <shabiq.ga@bts.id>
2025-07-12 15:51:12 +0700

    feat: home press mentions

--- components/home/HomeArticles.vue
+++ components/home/HomeArticles.vue
@@ -3,8 +3,8 @@
     id="home-articles"
     class="flex flex-col items-center px-5 md:px-20 py-20 gap-20 justify-center"
   >
-    <h2 class="font-bold text-4xl">Recent Articles</h2>
-    <div class="grid grid-cols-4 gap-10">
+    <h2 class="font-bold text-4xl text-center">Recent Articles</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
       <HomeArticleCard v-for="i in [1, 2, 3, 4]" :key="i" />
     </div>
     <NuxtLink href="/articles">
